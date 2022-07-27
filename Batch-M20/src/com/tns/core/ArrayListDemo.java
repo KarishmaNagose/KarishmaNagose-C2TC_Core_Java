f2eb0fb753f5b52542e2286c7e74b24ddc3e9eba
knago <knago@LAPTOP-UVEVNFA9>
2022-07-27 10:27:17 +0530

    ArrayList Demo2

--- Batch-M20/src/com/tns/core/ArrayListDemo.java
+++ Batch-M20/src/com/tns/core/ArrayListDemo.java
@@ -5,9 +5,9 @@ public class ArrayListDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            ArrayList l = new ArrayList();
-            l.add("A");
-            l.add(10);
+            ArrayList l = new ArrayList();//Growable in nature
+            l.add("A");//hetrogenous
+            l.add(10);//support of built in methods
             l.add(20);
             l.add(null);
             l.remove(2);
