bc7d1a8623932ae2e3b1a982aa19059663a084c2
knago <knago@LAPTOP-UVEVNFA9>
2022-07-27 10:26:50 +0530

    ArrayList DEmo2

--- Batch-M20/src/com/tns/core/ArrayListDemo.java
+++ Batch-M20/src/com/tns/core/ArrayListDemo.java
@@ -1,10 +1,19 @@
 package com.tns.core;
 
+import java.util.*;
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+            ArrayList l = new ArrayList();
+            l.add("A");
+            l.add(10);
+            l.add(20);
+            l.add(null);
+            l.remove(2);
+            l.add(2,"M");
+            l.add("N");
+            System.out.println(l);
 	}
 
 }
