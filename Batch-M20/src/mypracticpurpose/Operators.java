9c2e23a4209c33733b9b660bc8edafdfe9549c99
knago <knago@LAPTOP-UVEVNFA9>
2022-08-22 10:24:44 +0530

    Operators

---
+++ Batch-M20/src/mypracticpurpose/Operators.java
@@ -0,0 +1,18 @@
+package mypracticpurpose;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=10;
+		//Unary operators
+		/*System.out.println(a++);//11
+		System.out.println(++a);//12
+		System.out.println(a--);//
+		System.out.println(--a);*/
+
+
+		
+
+	}
+
+}
