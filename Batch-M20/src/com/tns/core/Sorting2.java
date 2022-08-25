8712bf5089a3dfebfc009de1326c7a405f7a836f
knago <knago@LAPTOP-UVEVNFA9>
2022-08-25 20:01:04 +0530

    Sorting

---
+++ Batch-M20/src/com/tns/core/Sorting2.java
@@ -0,0 +1,26 @@
+package com.tns.core;
+
+//Java program to demonstrate working of
+//sort() method of Arrays class
+
+//Importing Arrays class from java.util package
+import java.util.Arrays;
+
+//Main class
+public class Sorting2 {
+
+ // Main driver method
+ public static void main(String[] args)
+ {
+     // Custom input array
+     int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
+
+     // Calling the sort() method present
+     // inside Arrays class
+     Arrays.sort(arr);
+
+     // Printing and display sorted array
+     System.out.printf("Modified arr[] : %s",
+                       Arrays.toString(arr));
+ }
+}
