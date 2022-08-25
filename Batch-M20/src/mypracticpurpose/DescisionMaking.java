61e7d412ec5c43be29c66625f7b0366168bde85f
knago <knago@LAPTOP-UVEVNFA9>
2022-08-25 20:19:25 +0530

    practice

---
+++ Batch-M20/src/mypracticpurpose/DescisionMaking.java
@@ -0,0 +1,20 @@
+package mypracticpurpose;
+
+ class DescisionMaking {
+
+	public static void main(String[] args) {
+	try
+	{
+		int a=0;
+		int b=5;
+		int c=b/a;
+		
+		System.out.print("Hello");
+		}
+	catch(Exception e)
+	{
+		System.out.print("World");
+	}
+
+}
+}
